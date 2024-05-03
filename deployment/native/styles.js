051e9ec9d68657444b78fd64b2c23ec7731482eb
okngns <okngn1@outlook.com>
2024-05-03 09:16:48 +0200

    rest of the files

---
+++ deployment/native/styles.js
@@ -0,0 +1,12 @@
+import * as style0 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/themesource/administration/native/main";
+import * as style1 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/themesource/communitycommons/native/main";
+import * as style2 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/themesource/nanoflowcommons/native/main";
+import * as style3 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/themesource/webactions/native/main";
+import * as style4 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/themesource/atlas_core/native/main";
+import * as style5 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/themesource/feedbackmodule/native/main";
+import * as style6 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/themesource/masterdata/native/main";
+import * as style7 from "C:/Users/User/Mendix/20240325_Assessment_OkanGunes-main/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7]);
